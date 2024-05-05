8ee1c8250998708faf00b94fb78e5a4743e2f876
Jfeliz17 <Jfelizsorian@binghamton.edu>
2024-05-05 01:45:34 -0400

    Jay

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -28,10 +28,10 @@ class MainMenu:
         self.enemyS = pygame.transform.scale_by(self.enemy, 1)
         self.hyperEF = pygame.image.load('assets/hyperEF.png')
         self.hyperS= pygame.transform.scale_by(self.hyperEF, 0.2)
-        self.hyperR= pygame.transform.scale_by(self.hyperEF, 270)
+        self.hyperR= pygame.transform.scale_by(self.hyperEF, 2.3)
         self.fireEF = pygame.image.load('assets/fireEF.png')
         self.fireS = pygame.transform.scale_by(self.fireEF, 0.2)
-        self.fireR = pygame.transform.rotate(self.fireS, )
+        self.fireR = pygame.transform.rotate(self.fireS, 2.3)
 
         self.font = pygame.font.Font(None, 36)
         self.buttons = [
